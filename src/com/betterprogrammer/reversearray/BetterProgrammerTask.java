69b81f42e437ad51f01c77c22468f4a44ae444c9
Basyav <zbas@mail.ru>
2016-10-29 18:50:28 +0300

    Improved

--- src/com/betterprogrammer/reversearray/BetterProgrammerTask.java
+++ src/com/betterprogrammer/reversearray/BetterProgrammerTask.java
@@ -8,10 +8,9 @@ public class BetterProgrammerTask {
           return a new array where the order of elements has been reversed from the original
           array.
          */
-		int size = a.length;
-		Object[] b = new Object[size];
+		Object[] b = new Object[a.length];
 		int j = 0;
-		for (int i = size; i > 0; i--) {
+		for (int i = a.length; i > 0; i--) {
 			b[j] = a[i-1];
 			j++;
 		}
